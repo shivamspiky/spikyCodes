10cae826d6d25b3a64cd3ee88a175a5b530fadb7
Shivam Kumar <shivam.kumar@snapdeal.com>
2016-08-02 02:48:51 +0530

    formatting

--- DataStructuresAndAlgorithms/Sorting/BubbleSort/src/test/java/com/sorting/bubble/BubbleSortTest.java
+++ DataStructuresAndAlgorithms/Sorting/BubbleSort/src/test/java/com/sorting/bubble/BubbleSortTest.java
@@ -2,47 +2,50 @@ package com.sorting.bubble;
 
 import org.junit.Test;
 
+/**
+ * @author : shivam kumar
+ * 
+ *         Juints for BubbleSort Algorithm.
+ */
+
 public class BubbleSortTest {
 
 	@Test
 	public void sortTest() {
-		
-		
-		int[] arr = {4,2,0,3,9,2,5,17,11,15,19,16};
-		BubbleSort bubbleSort=new BubbleSort();
-		arr=bubbleSort.sort(arr);
-		
+
+		int[] arr = { 4, 2, 0, 3, 9, 2, 5, 17, 11, 15, 19, 16 };
+		BubbleSort bubbleSort = new BubbleSort();
+		arr = bubbleSort.sort(arr);
+
 		System.out.println("=============Sorted Array=============");
-		for(int i=0; i<arr.length;i++){
-			
+		for (int i = 0; i < arr.length; i++) {
+
 			System.out.println(arr[i] + " ");
 		}
 	}
-	
+
 	@Test
 	public void sortTestOneElementArray() {
-		
-		
-		int[] arr = {1};
-		BubbleSort bubbleSort=new BubbleSort();
-		arr=bubbleSort.sort(arr);
+
+		int[] arr = { 1 };
+		BubbleSort bubbleSort = new BubbleSort();
+		arr = bubbleSort.sort(arr);
 		System.out.println("=============Sorted Array=============");
-		for(int i=0; i<arr.length;i++){
-			
+		for (int i = 0; i < arr.length; i++) {
+
 			System.out.println(arr[i] + " ");
 		}
 	}
-	
+
 	@Test
 	public void sortTestEmptyArray() {
-		
-		
+
 		int[] arr = {};
-		BubbleSort bubbleSort=new BubbleSort();
-		arr=bubbleSort.sort(arr);
+		BubbleSort bubbleSort = new BubbleSort();
+		arr = bubbleSort.sort(arr);
 		System.out.println("=============Sorted Array=============");
-		for(int i=0; i<arr.length;i++){
-			
+		for (int i = 0; i < arr.length; i++) {
+
 			System.out.println(arr[i] + " ");
 		}
 	}
