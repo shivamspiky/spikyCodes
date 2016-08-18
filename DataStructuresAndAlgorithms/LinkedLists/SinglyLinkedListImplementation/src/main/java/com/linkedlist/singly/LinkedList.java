5609b5b7533b9d3186d848c89b379a5b46a1d2a2
Shivam Kumar <shivam.kumar@snapdeal.com>
2016-08-19 00:06:59 +0530

    linkedlist

---
+++ DataStructuresAndAlgorithms/LinkedLists/SinglyLinkedListImplementation/src/main/java/com/linkedlist/singly/LinkedList.java
@@ -0,0 +1,27 @@
+package com.linkedlist.singly;
+
+public class LinkedList {
+
+	private int length;
+	private ListNode head;
+
+	public LinkedList() {
+		this.length = 0;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
+
+	public void setHead(ListNode head) {
+		this.head = head;
+	}
+
+	public ListNode getHead() {
+		return this.head;
+	}
+}
