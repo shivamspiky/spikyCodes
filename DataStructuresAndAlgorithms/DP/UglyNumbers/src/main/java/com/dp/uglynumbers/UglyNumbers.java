92cf0580d603b2f12d8e671a8821635372a2be0f
Shivam Kumar <shivam.kumar@snapdeal.com>
2017-03-15 18:54:15 +0530

    optimised ugly number sequence

--- DataStructuresAndAlgorithms/DP/UglyNumbers/src/main/java/com/dp/uglynumbers/UglyNumbers.java
+++ DataStructuresAndAlgorithms/DP/UglyNumbers/src/main/java/com/dp/uglynumbers/UglyNumbers.java
@@ -3,41 +3,39 @@ package com.dp.uglynumbers;
 public class UglyNumbers {
 	
 	public int uglyNumber(int n){
-		int[] uglyNumbers = new int[n+1];
+		int[] uglyNumbers = new int[n];
 		uglyNumbers[0] = 1;
 		int i2=0;
 		int i3=0;
 		int i5=0;
-		int next_multiple_of_2 = uglyNumbers[i2]*2;
-		int next_multiple_of_3 = uglyNumbers[i3]*3;
-		int next_multiple_of_5 = uglyNumbers[i5]*5;
-		for(int i = 1; i< n+1 ;i++){
+//		int next_multiple_of_2 = uglyNumbers[i2]*2;
+//		int next_multiple_of_3 = uglyNumbers[i3]*3;
+//		int next_multiple_of_5 = uglyNumbers[i5]*5;
+		for(int i = 1; i< n ;i++){
 			
-			uglyNumbers[i] = Math.min(next_multiple_of_2, next_multiple_of_3);
-			uglyNumbers[i] = Math.min(uglyNumbers[i], next_multiple_of_5);
+			uglyNumbers[i] = Math.min(2*uglyNumbers[i2], 3*uglyNumbers[i3]);
+			uglyNumbers[i] = Math.min(uglyNumbers[i], 5*uglyNumbers[i5]);
 			
-			if(uglyNumbers[i] == next_multiple_of_2){
+			if(uglyNumbers[i] == 2*uglyNumbers[i2]){
 				i2++;
-				next_multiple_of_2= uglyNumbers[i2]*2;
+				
 			}
-			if(uglyNumbers[i] == next_multiple_of_3){
+			if(uglyNumbers[i] == 3*uglyNumbers[i3]){
 				i3++;
-				next_multiple_of_3= uglyNumbers[i3]*3;
+				
 			}
-			if(uglyNumbers[i] == next_multiple_of_5){
+			if(uglyNumbers[i] == 5*uglyNumbers[i5]){
 				i5++;
-				next_multiple_of_5= uglyNumbers[i5]*5;
 			}
 			
 		}
-		
-		return uglyNumbers[n];
+		return uglyNumbers[n-1];
 	}
 	
 	public static void main(String agrs[]){
 		
 		UglyNumbers uglyNumbers = new UglyNumbers();
-		int result = uglyNumbers.uglyNumber(150);
+		int result = uglyNumbers.uglyNumber(11);
 		System.out.println(result);
 	}
 
