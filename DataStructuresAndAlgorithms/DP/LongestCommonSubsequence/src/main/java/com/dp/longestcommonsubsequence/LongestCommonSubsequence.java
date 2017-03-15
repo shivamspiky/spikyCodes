5408bf0cce9e7bb42ca71df182899936daa44164
Shivam Kumar <shivam.kumar@snapdeal.com>
2017-03-15 20:53:06 +0530

    avoid calling length function of string in loop control

--- DataStructuresAndAlgorithms/DP/LongestCommonSubsequence/src/main/java/com/dp/longestcommonsubsequence/LongestCommonSubsequence.java
+++ DataStructuresAndAlgorithms/DP/LongestCommonSubsequence/src/main/java/com/dp/longestcommonsubsequence/LongestCommonSubsequence.java
@@ -39,7 +39,10 @@ public class LongestCommonSubsequence {
 			String str1, String str2) {
 		
 		int i=0,j=0;
-		while(i<str1.length() && j<str2.length()){
+		
+		int n = str1.length();
+		int m = str2.length();
+		while(i<n && j<m){
 			
 				if(str1.charAt(i) == str2.charAt(j)){
 					System.out.print(str1.charAt(i));
