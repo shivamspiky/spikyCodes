01f6b6ec50345881ffff0fdbae1600cabe5db4b0
Shivam Kumar <shivam.kumar@snapdeal.com>
2017-03-18 15:06:29 +0530

    two repeating elements with bit wise manipulation

--- DataStructuresAndAlgorithms/Arrays/FindTwoRepeatingElements/src/com/arrays/tworepeatingelements/TwoRepeatingElements.java
+++ DataStructuresAndAlgorithms/Arrays/FindTwoRepeatingElements/src/com/arrays/tworepeatingelements/TwoRepeatingElements.java
@@ -14,9 +14,42 @@ public class TwoRepeatingElements {
 		}
 	}
 	
+	public void repeatingElementsBits(int[] arr) {
+		
+		int xor=0;
+		
+		for(int i=0;i<arr.length;i++) {
+			xor^=arr[i];
+		}
+		
+		for(int i=1;i<arr.length-1;i++) {
+			xor^=i;
+		}
+		
+		int set_bit_number = xor^~(xor-1);
+		int k =0,l=0;
+		for(int i=0;i<arr.length;i++) {
+			if((set_bit_number & arr[i])!=0) {
+				k^=arr[i];
+			} else {
+				l^=arr[i];
+			}
+		}
+		
+		for(int i=1;i<arr.length-1;i++) {
+			if((set_bit_number & i)!=0) {
+				k^=i;
+			} else {
+				l^=i;
+			}
+		}
+		
+		System.out.println("Repeating elements are " + k + " " + l);
+	}
+	
 	public static void main(String[] args) {
 		TwoRepeatingElements twoRepeatingElements = new TwoRepeatingElements();
 		int arr[] = {4, 2, 4, 5, 2, 3, 1};
-		twoRepeatingElements.repeatingElements(arr);
+		twoRepeatingElements.repeatingElementsBits(arr);
 	}
 }
