3f7ed024398faaf32c3c92ab045db2c78fc7c983
Shivam Kumar <shivam.kumar@snapdeal.com>
2017-03-27 22:02:13 +0530

    iterative factorial method

--- DataStructuresAndAlgorithms/RecursionAndBacktracking/Factorial/src/main/java/com/recursion/factorial/Factorial.java
+++ DataStructuresAndAlgorithms/RecursionAndBacktracking/Factorial/src/main/java/com/recursion/factorial/Factorial.java
@@ -10,4 +10,17 @@ public class Factorial {
 		
 		return n*fact(n-1);
 	}
+	
+	public int iterativeFact(int n) {
+		if(n==0 || n==1) {
+			return 1;
+		}
+		
+		int fact=1;
+		for(int i=2;i<=n;i++) {
+			fact = fact * i;
+		}
+		
+		return fact;
+	}
 }
--- DataStructuresAndAlgorithms/RecursionAndBacktracking/Factorial/src/test/java/com/recursion/factorial/FactorialTest.java
+++ DataStructuresAndAlgorithms/RecursionAndBacktracking/Factorial/src/test/java/com/recursion/factorial/FactorialTest.java
@@ -7,8 +7,10 @@ public class FactorialTest {
 	@Test
 	public void factTest() {
 		Factorial factorial = new Factorial();
-		int result=factorial.fact(5);
+		int result=factorial.fact(6);
+		int result1=factorial.iterativeFact(6);
 		System.out.println(result);
+		System.out.println(result1);
 	}
 
 }
