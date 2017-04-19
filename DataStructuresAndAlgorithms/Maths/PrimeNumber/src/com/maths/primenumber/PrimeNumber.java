e8289264efae263d492e8b498483cb0809592fa8
Shivam Kumar <shivam.kumar@snapdeal.com>
2017-04-19 09:26:55 +0530

    explanation of prime number check algorithm

--- DataStructuresAndAlgorithms/Maths/PrimeNumber/src/com/maths/primenumber/PrimeNumber.java
+++ DataStructuresAndAlgorithms/Maths/PrimeNumber/src/com/maths/primenumber/PrimeNumber.java
@@ -1,6 +1,17 @@
 package com.maths.primenumber;
 
 public class PrimeNumber {
+	
+	/*A number divisible by something greater than 
+	 * its square-root will definately be divisible
+	 * by number smaller than its square-root
+	 * so that product of these two calculates to n
+	 * if still not clear
+	 * let's look two equations
+	 * one with a equals to product of its square root c * c
+	 * and other a equals to product of b (which is greater than c) * k 
+	 * then one of number is already greater than c so for product to be equal
+	 * k has to be smaller than c so that their product calculates to a*/
 	public boolean prime(int n) {
 		
 		for(int i=2;i*i<=n;i++) {
