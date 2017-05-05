6d0211f9434b5f2a25f5a0788426e08f5763eb7c
Shivam Kumar <shivam.kumar@snapdeal.com>
2017-05-05 19:02:51 +0530

    balanced parenthesis reloaded

--- DataStructuresAndAlgorithms/Stack/BalancedParanthesis/src/main/java/com/stack/balancedparanthesis/BalancedParanthesis.java
+++ DataStructuresAndAlgorithms/Stack/BalancedParanthesis/src/main/java/com/stack/balancedparanthesis/BalancedParanthesis.java
@@ -7,30 +7,25 @@ public class BalancedParanthesis {
 	public boolean checkBalancedParanthesis(String s) {
 
 		if ( s == null || s.length() == 0 ) {
-			return false;
+			return true;
 		}
 		Stack<Character> stack = new Stack<Character>();
 		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == ')') {
-				if (!stack.isEmpty() && stack.peek() == '(') {
-					stack.pop();
-				} else {
-					return false;
-				}
-			} else if (s.charAt(i) == '}') {
-				if (!stack.isEmpty() && stack.peek() == '{') {
-					stack.pop();
-				} else {
-					return false;
-				}
-			} else if (s.charAt(i) == ']') {
-				if (!stack.isEmpty() && stack.peek() == '[') {
-					stack.pop();
-				} else {
-					return false;
-				}
-			} else {
+			
+			if(matchesOpen(s.charAt(i))) {
 				stack.push(s.charAt(i));
+			}
+			
+			else if(matchesClose(s.charAt(i))) {
+				if(stack.isEmpty()) {
+					return false;
+				}
+				
+				char temp = stack.pop();
+				if(!((temp=='(' && s.charAt(i)==')') || (temp=='{' && s.charAt(i)=='}') 
+						|| (temp=='[' && s.charAt(i)==']'))) {
+					return false;
+				}
 			}
 		}
 
@@ -41,11 +36,33 @@ public class BalancedParanthesis {
 		}
 
 	}
+	
+	private boolean matchesOpen(char ch) {
+		if(ch=='(' || ch=='{' || ch=='[') {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean matchesClose(char ch) {
+		if(ch==')' || ch=='}' || ch==']') {
+			return true;
+		}
+		
+		return false;
+	}
 
 	public static void main(String args[]) {
 		BalancedParanthesis balancedParanthesis = new BalancedParanthesis();
-		boolean result = balancedParanthesis.checkBalancedParanthesis(null);
-		System.out.println(result);
+		
+		System.out.println(balancedParanthesis.checkBalancedParanthesis("(aa)bb"));
+		System.out.println(balancedParanthesis.checkBalancedParanthesis("[aa [ bbb ]"));
+		System.out.println(balancedParanthesis.checkBalancedParanthesis("aa } bbb { c"));
+		System.out.println(balancedParanthesis.checkBalancedParanthesis("aa [ bb () ]"));
+		System.out.println(balancedParanthesis.checkBalancedParanthesis("[aa { ] cc } ]"));
+		
+		
 	}
 
 }
